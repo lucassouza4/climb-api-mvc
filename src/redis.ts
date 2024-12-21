f1b4a4f5cbbc13da61da005c66281e711f6fa85e
lucassouza4 <lussilva1998@gmail.com>
2024-12-20 23:44:36 -0300

    fix: configuração do redis

--- src/redis.ts
+++ src/redis.ts
@@ -4,6 +4,7 @@ const createRedisClient = (): Redis => {
   const client = new Redis({
     host: process.env.REDIS_HOST || "127.0.0.1",
     port: parseInt(process.env.REDIS_PORT || "6379", 10),
+    password: process.env.REDIS_PASSWORD || "",
   });
 
   client.on("connect", () => {
